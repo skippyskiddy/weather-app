c69ad67d9a63d8cbc1e20e762f092dcd4459bff0
Elif <elifdaa@gmail.com>
2023-07-28 17:46:45 -0400

    added requirements.txt and added clear documentation for each file

--- weather_api/api_weather_request.py
+++ weather_api/api_weather_request.py
@@ -1,48 +1,71 @@
+# Required modules
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from datetime import datetime
 import requests
 import pytz
 
+# Create a new Flask web server application
 app = Flask(__name__)
+# Enabling Cross Origin Resource Sharing for the server 
+# to allow other servers to access the API
 CORS(app, origins=["http://localhost:5173"])
 
+# Defining a route for the '/weather' URL, for GET requests
 @app.route('/weather', methods=['GET'])
 def get_weather_for_location():
+    # API keys for the OpenCageData and OpenWeatherMap services
+    # Please fill in your own API keys here 
     opencage_api_key = 'insert-your-opencagedata-api-key-here'
     openweathermap_api_key = 'insert-your-openweathermap-api-key-here'
 
+    # Extracts the 'location' parameter from the GET request
     location = request.args.get('location')
 
-    try:
+    try: # basic error handling
+        # making a request to the OpenCageData API to get the lat and lon for the location
         response = requests.get(f"https://api.opencagedata.com/geocode/v1/json?q={location}&key={opencage_api_key}")
         data = response.json()
 
+        # Checking if the API call was successful
         if response.status_code != 200:
             return jsonify({'error': f"An error occurred with the OpenCageData API: {data['status']['message']}"}), response.status_code
 
+        # Checking if the location exists in the OpenCageData API response
         if 'results' not in data or len(data['results']) == 0:
             return jsonify({'error': 'Location not found'}), 404
 
+        # Extracting the latitude and longitude
         lat, lon = data['results'][0]['geometry']['lat'], data['results'][0]['geometry']['lng']
 
+        # Making a request to the OpenWeatherMap API to get the weather for the location
         response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={openweathermap_api_key}")
         weather_data = response.json()
 
+        # Checking if the API call was successful
         if response.status_code != 200:
             return jsonify({'error': 'An error occurred with the OpenWeatherMap API'}), response.status_code
 
+        # Extracting and converting the temperature from Kelvin (from openweathermap) to Celsius
         temp_celsius = weather_data['main']['temp'] - 273.15
+        # Extracting the general weather condition and the more specific description
         condition = weather_data['weather'][0]['main']
         description = weather_data['weather'][0]['description']
+        # Extracting the timezone offset from UTC
         get_timezone_offset = weather_data['timezone']
 
+        # Converting the timezone offset from seconds to hours and then to minutes
         timezone_offset_hours = get_timezone_offset / 3600
         timezone_offset_minutes = timezone_offset_hours * 60
+
+        # Getting the current UTC time using pytz
         utc_time = datetime.now(pytz.utc)
+        # Adjusting the UTC time for the timezone offset to get the local time
         local_time = utc_time.astimezone(pytz.FixedOffset(timezone_offset_minutes))
+        # Formatting the local time
         formatted_local_time = local_time.strftime('%H:%M %p')
 
+        # Returning the weather data as a JSON response
         return jsonify({
             'location': location,
             'local_time': formatted_local_time,
@@ -51,7 +74,9 @@ def get_weather_for_location():
             'description': description
         })
     except Exception as e:
+        # If an error occurred, returns a 500 status code and a JSON with the error message
         return jsonify({'error': 'An error occurred while processing your request'}), 500
 
+# Runs the server in debug mode as its not in prod 
 if __name__ == "__main__":
     app.run(debug=True)
--- weather_ui/src/App.vue
+++ weather_ui/src/App.vue
@@ -1,51 +1,67 @@
 <template>
+  <!-- The main application container -->
   <div id="app" class="center">
+    <!-- The main application header -->
     <h1 class="header">Wanna know the weather, anywhere?</h1>
+    <!-- The text input for the location, bound to the location variable -->
     <input v-model="location" type="text" placeholder="Enter a location" class="input">
+    <!-- The button that triggers the fetchWeather function -->
     <button @click="fetchWeather">Get Weather</button>
+    <!-- Conditional rendering of the weather data if it exists -->
     <div v-if="weatherData">
       <h2>{{ weatherData.location }}</h2>
       <p>Local time: {{ weatherData.local_time }}</p>
       <p>Temperature: {{ weatherData.temperature }}</p>
       <p>Condition: {{ weatherData.condition }} ({{ weatherData.description }})</p>
     </div>
+    <!-- Conditional rendering of the error message if it exists -->
     <p v-if="errorMessage">{{ errorMessage }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
+// Importing required modules
 import axios from 'axios';
 import { ref } from 'vue';
 
+// Defining the structure of the weather data
 type WeatherData = {
   location: string;
-    local_time: string;
-    temperature: number;
-    condition: string;
-    description: string;
+  local_time: string;
+  temperature: number;
+  condition: string;
+  description: string;
 }
 
+// Setting up reactive variables
 const weatherData = ref<WeatherData | null>(null);
 const location = ref('')
 const errorMessage = ref('');
 
+// Function to fetch the weather data for the given location
 const fetchWeather = async () => {
   try {
-        const response = await axios.get<WeatherData>(`http://127.0.0.1:5000/weather?location=${location.value}`);
-        weatherData.value = response.data;
-        errorMessage.value = '';
-      } catch (error) {
-        if (error instanceof Error) {
-          weatherData.value = null
-          errorMessage.value = error.message;
-        } else {
-          throw error;
-        }
-      }
+    // Making a GET request to the weather API
+    const response = await axios.get<WeatherData>(`http://127.0.0.1:5000/weather?location=${location.value}`);
+    // Storing the returned data
+    weatherData.value = response.data;
+    // Clearing any previous error message
+    errorMessage.value = '';
+  } catch (error) {
+    // If an error occurred, clear the weather data and store the error message
+    if (error instanceof Error) {
+      weatherData.value = null
+      errorMessage.value = error.message;
+    } else {
+      // If the error is not an instance of Error, re-throw it
+      throw error;
+    }
+  }
 }
 </script>
 
 <style>
+/* Centering the content in the main application container */
 #app {
   text-align: center;
   margin-top: 60px;
@@ -58,10 +74,12 @@ const fetchWeather = async () => {
   flex-direction: column;
 }
 
+/* Adding a bottom margin to the header */
 .header {
   margin-bottom: 20px;
 }
 
+/* Adding a bottom margin to the input */
 .input {
   margin-bottom: 20px;
 }
